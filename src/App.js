952ffda377e0bffaf795d6bca145c3f8882591d4
Abdulmajeed Kutaibani <Abodymeo@yahoo.com>
2021-08-01 19:50:04 +0300

    make fonts and colors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1>rejgjrbbgiopdebfojfdoipbjopdfjbf</h1>
+        <h2>gejroighkjreoirhjoljrholrejr</h2>
+        <h3>i43uju3445yu098y04u5yu</h3>
+        <h4>ncv,mxcbnv,z/,c.vm.,blcnbn.mbnldnl;k</h4>
+        <p>
+          iugghiuerhogioepofgpoierjgt0i9u0tu54687u4jfkjdfnbnposejfio[erjhghoprhjrt
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
