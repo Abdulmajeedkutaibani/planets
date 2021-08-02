bf1b2d7919c4c8d002815f5e3a28d36549528ca3
Abdulmajeed Kutaibani <Abodymeo@yahoo.com>
2021-08-02 20:26:33 +0300

    finish styling tablet layout

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,20 +8,22 @@ const Hero = () => {
         <img src={mars} alt='' />
       </div>
 
-      <div className='planet-info'>
-        <h1>mars</h1>
-        <p className='planet-description'>
-          Mars is the fourth planet from the Sun and the second-smallest planet
-          in the Solar System, being larger than only Mercury. In English, Mars
-          carries the name of the Roman god of war and is often referred to as
-          the "Red Planet".
-        </p>
-        <p className='source'>Source : Wikipedia</p>
-        <Button />
-        <br />
-        <Button />
-        <br />
-        <Button />
+      <div className='planet'>
+        <div className='planet-info'>
+          <h1>mars</h1>
+          <p className='planet-description'>
+            Mars is the fourth planet from the Sun and the second-smallest
+            planet in the Solar System, being larger than only Mercury. In
+            English, Mars carries the name of the Roman god of war and is often
+            referred to as the "Red Planet".
+          </p>
+          <p className='source'>Source : Wikipedia</p>
+        </div>
+        <div className='info-btns'>
+          <Button />
+          <Button />
+          <Button />
+        </div>
       </div>
     </div>
   );
