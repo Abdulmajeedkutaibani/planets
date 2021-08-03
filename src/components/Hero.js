85e0006c574e7fd5ca4bc2d997862caa56a54f5c
Abdulmajeed Kutaibani <Abodymeo@yahoo.com>
2021-08-03 23:03:49 +0300

    create menu-btn and onClick function

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import mars from './assets/planet-mars-internal.svg';
 import Button from './Button';
+import planets from '../data.json';
 const Hero = () => {
   return (
     <div className='hero-container'>
@@ -10,14 +11,9 @@ const Hero = () => {
 
       <div className='planet'>
         <div className='planet-info'>
-          <h1>mars</h1>
-          <p className='planet-description'>
-            Mars is the fourth planet from the Sun and the second-smallest
-            planet in the Solar System, being larger than only Mercury. In
-            English, Mars carries the name of the Roman god of war and is often
-            referred to as the "Red Planet".
-          </p>
-          <p className='source'>Source : Wikipedia</p>
+          <h1>{planets[0].name}</h1>
+          <p className='planet-description'>{planets[0].overview.content}</p>
+          <p className='source'>{planets[0].overview.source}</p>
         </div>
         <div className='info-btns'>
           <Button btnText='overview' />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import hamburger from './assets/icon-hamburger.svg';
+import chevron from './assets/icon-chevron.svg';
 
 const Navbar = () => {
+  const handleClick = () => {
+    const menuBtn = document.querySelector('.menu-btn');
+    const navList = document.querySelector('.navList');
+
+    menuBtn.style.opacity = '0.5';
+    navList.style.display = 'flex';
+  };
   return (
     <nav>
       <h2>
@@ -9,45 +18,64 @@ const Navbar = () => {
         </a>
       </h2>
 
-      <ul>
+      <div className='menu-btn' onClick={handleClick}>
+        <img src={hamburger} alt='' />
+      </div>
+      <ul className='navList'>
         <li>
           <a href='#'>
+            <div className='planet-icon mercury'></div>
             <h4>mercury</h4>
+            <img src={chevron} alt='' />
           </a>
         </li>
         <li>
           <a href='#'>
+            <div className='planet-icon venus'></div>
             <h4>venus</h4>
+            <img src={chevron} alt='' />
           </a>
         </li>
         <li>
           <a href='#'>
+            <div className='planet-icon earth'></div>
             <h4>earth</h4>
+            <img src={chevron} alt='' />
           </a>
         </li>
         <li>
           <a href='#'>
+            <div className='planet-icon mars'></div>
             <h4>mars</h4>
+            <img src={chevron} alt='' />
           </a>
         </li>
         <li>
           <a href='#'>
+            <div className='planet-icon jupiter'></div>
             <h4>jupiter</h4>
+            <img src={chevron} alt='' />
           </a>
         </li>
         <li>
           <a href='#'>
+            <div className='planet-icon saturn'></div>
             <h4>saturn</h4>
+            <img src={chevron} alt='' />
           </a>
         </li>
         <li>
           <a href='#'>
+            <div className='planet-icon uranus'></div>
             <h4>uranus</h4>
+            <img src={chevron} alt='' />
           </a>
         </li>
         <li>
           <a href='#'>
+            <div className='planet-icon neptune'></div>
             <h4>neptune</h4>
+            <img src={chevron} alt='' />
           </a>
         </li>
       </ul>
